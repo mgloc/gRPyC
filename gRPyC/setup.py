7b61f9e763a9b819a87c3b981ad5ad756d38cdc4
gloxounet <matteo.glock@laposte.net>
2022-09-23 12:50:17 +0200

    ready to PyPi

--- gRPyC/setup.py
+++ gRPyC/setup.py
@@ -1,8 +1,14 @@
-from setuptools import setup
+from setuptools import setup,find_packages
 
 setup(
     name="grpyc",
     version='0.1',
+    license='MIT',
+    author="gloxounet",
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    url='https://github.com/Gloxounet/gRPyC',
+    keywords='gRPC framework',
     py_modules=['cli'],
     install_requires=[
         'Click',
